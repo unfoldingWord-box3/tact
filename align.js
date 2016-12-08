f364b224381a9b42bc1fc1d3d726331645172ff5
Christopher Klapp <christopher@klapp.name>
2016-12-08 10:24:35 -0500

    remove semicolons and unneeded configs

--- align.js
+++ align.js
@@ -1,28 +1,23 @@
 #!/usr/bin/env node
-var tact = require('./tact/tact.js');
+var tact = require('./tact/tact.js')
 
-var cli = require('cli');
+var cli = require('cli')
 var options = cli.parse({
-  sourceLanguage: ['x', 'source language name', 'string', ''],
-  targetLanguage: ['y', 'target language name', 'string', ''],
   sourceFile: ['s', 'source file to be aligned', 'file', ''],
   targetFile: ['t', 'target file to be aligned', 'file', ''],
   first: ['f', 'first input line to use', 'int', 0],
   count: ['n', 'limit input to n lines', 'int', 20]
-});
-
-tact.config.global.sourceLanguage = options.sourceLanguage
-tact.config.global.targetLanguage = options.targetLanguage
+})
 
 function progress(percent) {
-  cli.progress(percent);
-};
+  cli.progress(percent)
+}
 
 tact.corpus.parseFiles(options.sourceFile, options.targetFile, function(alignmentPairs) {
-  alignmentPairs = alignmentPairs.slice(options.first,options.first+options.count);
-  // console.time('alignment-1');
+  alignmentPairs = alignmentPairs.slice(options.first,options.first+options.count)
+  // console.time('alignment-1')
   tact.aligning.align(alignmentPairs, progress, function(alignments) {
-    console.log(JSON.stringify(alignments, null, 2));
-    // console.timeEnd('alignment-1');
-  });
-});
+    console.log(JSON.stringify(alignments, null, 2))
+    // console.timeEnd('alignment-1')
+  })
+})
--- train.js
+++ train.js
@@ -1,40 +1,35 @@
 #!/usr/bin/env node --max_old_space_size=4096
-var tact = require('./tact/tact.js');
-var async = require('async');
+var tact = require('./tact/tact.js')
+var async = require('async')
 
-var cli = require('cli');
+var cli = require('cli')
 var options = cli.parse({
-  sourceLanguage: ['x', 'source language name', 'string', ''],
-  targetLanguage: ['y', 'target language name', 'string', ''],
   sourceCorpusFile: ['s', 'source corpus file to train', 'file', './corpus/source.txt'],
   targetCorpusFile: ['t', 'target corpus file to be aligned', 'file', './corpus/target.txt'],
   sourceCorrectionsFile: ['a', 'source corrections file to train', 'file', './corrections/source.txt'],
   targetCorrectionsFile: ['b', 'target corrections file to be aligned', 'file', './corrections/target.txt']
-});
-
-tact.config.global.sourceLanguage = options.sourceLanguage
-tact.config.global.targetLanguage = options.targetLanguage
+})
 
 function progress(percent) {
-  cli.progress(percent);
-};
+  cli.progress(percent)
+}
 
 tact.corpus.parseFiles(options.sourceCorrectionsFile, options.targetCorrectionsFile, function(corrections) {
-  // console.time('training-1');
-  // console.time('correctionsTable-1');
+  // console.time('training-1')
+  // console.time('correctionsTable-1')
   tact.corpus.parseFiles(options.sourceCorpusFile, options.targetCorpusFile, function(corpus) {
-    // console.time('corpusTable-1');
+    // console.time('corpusTable-1')
     tact.training.train(corpus, corrections, progress, progress,
       function() {
-        // console.timeEnd('correctionsTable-1');
+        // console.timeEnd('correctionsTable-1')
       },
       function() {
-        // console.timeEnd('corpusTable-1');
+        // console.timeEnd('corpusTable-1')
       },
       function() {
-        // console.timeEnd('training-1');
-        console.log('trained'); // output here helps callback return and not hang.
+        // console.timeEnd('training-1')
+        console.log('trained') // output here helps callback return and not hang.
       }
-    );
-  });
-});
+    )
+  })
+})
